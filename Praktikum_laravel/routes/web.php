1f158509e10e0c4076de1cbe231e978b8ba95f4d
Gusti Muhammad Akmal Kesuma <c030325122@mahasiswa.poliban.ac.id>
2026-09-17 17:07:03 +0800

    Menambahkan fitur tampil daftar mahasiswa dan perbaikan model fillable

--- Praktikum_laravel/routes/web.php
+++ Praktikum_laravel/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Tambahkan route ini:
+Route::resource('mahasiswa', MahasiswaController::class);
